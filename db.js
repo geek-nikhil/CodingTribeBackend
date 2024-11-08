d8e2c5f2d84aa1e093067f339e1f3791e526fd51
Nikhil Raikwar <nikhilraikwar846@gmail.com>
2024-11-08 18:06:02 +0530

    mongodb issue solved

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://nikhilraikwar846:iB7DrCv2H6aqtRFm@cluster0.a8u4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-
+mongoose.connect('mongodb+srv://nikhilraikwar846:jYtgyiDPV56SfaPY@cluster0.a8u4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
 })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.error('Failed to connect to MongoDB:', err));
